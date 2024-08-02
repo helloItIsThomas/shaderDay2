d7ea76b0e456b9be63cb925c7755d9b6fdcaf99d
Thomas McElmeel <thomasmcelmeel@Thomass-MacBook-Pro.local>
2024-08-01 22:34:47 -0400

    ~

---
+++ main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const c = document.getElementById("glslCanvas");
+  var sandbox = new GlslCanvas(c);
+
+  fetch("shaders/shader.frag")
+    .then((response) => response.text())
+    .then((data) => {
+      sandbox.load(data);
+      sandbox.setUniform("u_debug", 0.15);
+    })
+    .catch((error) => console.error("Error loading shader:", error));
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+}
+animate();
--- shaders/shader.frag
+++ shaders/shader.frag
@@ -2,6 +2,13 @@
 precision mediump float;
 #endif
 
-void main() {
-    gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
+uniform float u_time;
+uniform float u_debug;
+
+float something() {
+    return 0.5 + 0.5 * sin(u_time * 5.0);
 }
+
+void main() {
+    gl_FragColor = vec4(something(), u_debug, 0.0, 1.0);
+}  
